9aea4eaeb19e60d6f1573f939d0d1815c979cc5f
mickeyc0309 <chenmickey0309@gmail.com>
2022-04-03 23:09:12 -0400

    rest-api files ready to be hosted

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+from flask_cors import CORS
+from ML_recommender import recListMovie
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/movie", methods=["POST"])
+def movie_recs_list():
+    try:
+        out = {}
+        ids = request.args.get("ids") # get the comma separated ids
+        ids = ids.split(",") # split the ids with ,
+        for i in range(len(ids)):
+            ids[i] = int(ids[i].strip()) # removing unnecesary spaces
+        movie100 = recListMovie(ids)
+        movie100 = list(map(int, movie100))
+        out["recommendation"] = movie100
+        return out
+    except IndexError as err:
+        return {"error": f"Maybe movie_ID does not exist in database. {err}"}
+
+@app.route('/')
+def textServer():
+    return "<h2>The server works!</h2>"        
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=True)
